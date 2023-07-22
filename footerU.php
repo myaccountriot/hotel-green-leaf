1a23457b19304d6d03e1593a8b3c75dcbafb4ae2
myaccountriot <135790022+myaccountriot@users.noreply.github.com>
2023-07-22 22:02:56 +0530

    user details

---
+++ footerU.php
@@ -0,0 +1,15 @@
+
+<div class="row">
+            <h5><?php echo"Copyright <span>&copy</span> Green Leaf 2023,All rights reserved <span>&#174</span>" ?></h5>
+        </div>
+</div>
+
+<!-- style link -->
+<link rel="stylesheet" href="UserStyle.css">
+
+<!-- Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
